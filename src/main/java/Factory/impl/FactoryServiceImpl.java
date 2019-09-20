fd69ba87dcbefa41ef66dc8476638f78dfc49549
jianjiu.xc <jianjiu.xc@koubei.com>
2019-09-20 13:22:01 +0800

    工程模式初始化

---
+++ src/main/java/Factory/impl/FactoryServiceImpl.java
@@ -0,0 +1,13 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2019 All Rights Reserved.
+ */
+package Factory.impl;
+
+/**
+ *
+ * @author jianjiu
+ * @version : FactoryServiceImpl.java, v 0.1 2019Äê09ÔÂ20ÈÕ 13:19 jianjiu Exp $
+ */
+public class FactoryServiceImpl {
+}
---
+++ src/main/java/Factory/test/FactoryServiceTest.java
@@ -0,0 +1,13 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2019 All Rights Reserved.
+ */
+package Factory.test;
+
+/**
+ *
+ * @author jianjiu
+ * @version : FactoryServiceTest.java, v 0.1 2019Äê09ÔÂ20ÈÕ 13:20 jianjiu Exp $
+ */
+public class FactoryServiceTest {
+}
